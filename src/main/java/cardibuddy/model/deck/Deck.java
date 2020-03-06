bbab994f878564e3f04a2b81ea5fc7699a448d07
Jing Ying <e0323887@u.nus.edu>
2020-03-06 14:33:13 +0800

    Edited Deck, Title, Question, Answer and Flashcard classes

--- src/main/java/cardibuddy/model/deck/Deck.java
+++ src/main/java/cardibuddy/model/deck/Deck.java
@@ -2,6 +2,7 @@ package cardibuddy.model.deck;
 
 import static cardibuddy.commons.util.CollectionUtil.requireAllNonNull;
 
+import cardibuddy.model.flashcard.Flashcard;
 import cardibuddy.model.tag.Tag;
 import java.util.Collections;
 import java.util.HashSet;
@@ -19,6 +20,7 @@ public class Deck {
 
     // Data fields
     private final Set<Tag> tags = new HashSet<>();
+    private Set<Flashcard> flashcards = new HashSet<>();
 
     /**
      * Every field must be present and not null.
@@ -29,7 +31,7 @@ public class Deck {
         this.tags.addAll(tags);
     }
 
-    public Title getTitle() { return title; }
+    public Title getTitle() { return this.title; }
 
     /**
      * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
@@ -38,5 +40,50 @@ public class Deck {
     public Set<Tag> getTags() {
         return Collections.unmodifiableSet(tags);
     }
+
+    public Set<Flashcard> getFlashcards() {
+        return this.flashcards;
+    }
+
+    public Set<Flashcard> addFlashcard(Flashcard card) {
+        flashcards.add(card);
+        return Collections.unmodifiableSet(flashcards);
+    }
+
+    /**
+     * Returns true if both decks have the same identity and data fields.
+     * This defines a stronger notion of equality between two decks.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Deck)) {
+            return false;
+        }
+
+        Deck otherDeck = (Deck) other;
+        return otherDeck.getTitle().equals(getTitle())
+                && otherDeck.getTags().equals(getTags())
+                && otherDeck.getFlashcards().equals(getFlashcards());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(title, flashcards, tags);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getTitle())
+                .append(" Tags: ");
+        getTags().forEach(builder::append);
+        return builder.toString();
+    }
+
 
 }
