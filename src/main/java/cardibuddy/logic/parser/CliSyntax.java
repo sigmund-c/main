05a36972f4329cbf2015f49e09f68b60054df79f
phaebaez <50545929+phaebaez@users.noreply.github.com>
2020-03-12 11:43:54 +0800

    edited AddCommand

--- src/main/java/cardibuddy/logic/parser/CliSyntax.java
+++ src/main/java/cardibuddy/logic/parser/CliSyntax.java
@@ -8,5 +8,6 @@ public class CliSyntax {
     /* Prefix definitions */
     public static final Prefix PREFIX_TITLE = new Prefix("n/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-
+    public static final Prefix PREFIX_Deck = new Prefix("d/");
+    public static final Prefix PREFIX_Flashcard = new Prefix("f/");
 }
