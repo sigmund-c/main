fda37a831ac317cbfe9d904bb81e7a832f20b3a5
phaebaez <50545929+phaebaez@users.noreply.github.com>
2020-03-16 20:08:47 +0800

    fixed styling errors

--- src/main/java/cardibuddy/logic/commands/ListCommand.java
+++ src/main/java/cardibuddy/logic/commands/ListCommand.java
@@ -6,18 +6,18 @@ import static java.util.Objects.requireNonNull;
 import cardibuddy.logic.commands.exceptions.CommandException;
 import cardibuddy.model.Model;
 
-/**
- * Lists all decks/flashcards in the directory to the user.
- */
+    /**
+    * Lists all decks/flashcards in the directory to the user.
+    */
     public class ListCommand extends Command {
-    public static final String COMMAND_WORD = "list";
+        public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all decks";
+        public static final String MESSAGE_SUCCESS = "Listed all decks";
 
-    @Override
-    public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
-        model.updateFilteredDeckList(PREDICATE_SHOW_ALL_DECKS);
-        return new CommandResult(MESSAGE_SUCCESS);
-    }
+        @Override
+        public CommandResult execute(Model model) throws CommandException {
+            requireNonNull(model);
+            model.updateFilteredDeckList(PREDICATE_SHOW_ALL_DECKS);
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
 }
