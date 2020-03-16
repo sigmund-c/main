1b4aed780e0c0749948690b5e4cfd6352a710cb8
phaebaez <50545929+phaebaez@users.noreply.github.com>
2020-03-16 17:24:29 +0800

    Added back PREFIX_TITLE

--- src/main/java/cardibuddy/logic/parser/CliSyntax.java
+++ src/main/java/cardibuddy/logic/parser/CliSyntax.java
@@ -6,7 +6,7 @@ package cardibuddy.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-//    public static final Prefix PREFIX_TITLE = new Prefix("n/");
+    public static final Prefix PREFIX_TITLE = new Prefix("n/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_DECK = new Prefix("d/");
     public static final Prefix PREFIX_FLASHCARD = new Prefix("c/");
