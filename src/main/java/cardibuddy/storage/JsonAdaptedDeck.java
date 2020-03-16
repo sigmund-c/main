28e524ee58d5dd040c513df31c1679a1604c90c9
Ashleigh <ashleighaathar@gmail.com>
2020-03-16 23:39:16 +0800

    add JsonAdaptedDeck, modify other files in Storage

---
+++ src/main/java/cardibuddy/storage/JsonAdaptedDeck.java
@@ -0,0 +1,76 @@
+package cardibuddy.storage;
+
+import cardibuddy.commons.exceptions.IllegalValueException;
+import cardibuddy.model.deck.Deck;
+import cardibuddy.model.flashcard.Flashcard;
+import cardibuddy.model.tag.Tag;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class JsonAdaptedDeck extends JsonAdaptedView{
+
+    /**
+     * Jackson-friendly version of {@link Deck}.
+     */
+
+        public static final String MISSING_FIELD_MESSAGE_FORMAT = "Deck's %s field is missing!";
+
+        private final String title;
+        private final List<JsonAdaptedFlashcard> flashcards = new ArrayList<>();
+        private final List<JsonAdaptedTag> tagged = new ArrayList<>();
+
+        /**
+         * Constructs a {@code JsonAdaptedFlashcard} with the given flashcard details.
+         */
+        @JsonCreator
+        public JsonAdaptedDeck(@JsonProperty("title") String title, @JsonProperty("flashcards") List<JsonAdaptedFlashcard> flashcards,
+                                    @JsonProperty("tagged") List<cardibuddy.storage.JsonAdaptedTag> tagged) {
+            this.title = title;
+            this.flashcards.addAll(flashcards);
+            if (tagged != null) {
+                this.tagged.addAll(tagged);
+            }
+        }
+
+        /**
+         * Converts a given {@code Deck} into this class for Jackson use.
+         */
+        public JsonAdaptedDeck(Deck source) {
+            this.title = source.getTitle().toString();
+            flashcards.addAll(source.getFlashcards().stream().map(cardibuddy.storage.JsonAdaptedFlashcard::new).collect(Collectors.toList()));
+            tagged.addAll(source.getTags().stream()
+                    .map(cardibuddy.storage.JsonAdaptedTag::new)
+                    .collect(Collectors.toList()));
+        }
+
+        /**
+         * Converts this Jackson-friendly adapted flashcard object into the model's {@code Flashcard} object.
+         *
+         * @throws IllegalValueException if there were any data constraints violated in the adapted flashcard.
+         */
+        public Deck toModelType() throws IllegalValueException {
+            final List<Tag> deckTags = new ArrayList<>();
+            for (cardibuddy.storage.JsonAdaptedTag tag : tagged) {
+                deckTags.add(tag.toModelType());
+            }
+
+            final List<Flashcard> deckFlashcards = new ArrayList<>();
+            for (JsonAdaptedFlashcard flashcard : flashcards) {
+                deckFlashcards.add(flashcard.toModelType());
+            }
+
+            // TODO: add if conditions here to check formatting
+
+            final Set<Tag> modelTags = new HashSet<>(deckTags);
+            return new Deck(null, null); // TODO: to replace params with actual values
+        }
+
+    }
+
+}
