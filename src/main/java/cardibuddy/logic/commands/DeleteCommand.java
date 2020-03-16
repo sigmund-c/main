9816ee7f6787982e72bbf93b630569d461fe9971
phaebaez <50545929+phaebaez@users.noreply.github.com>
2020-03-16 15:49:13 +0800

    Edited DeleteCommand

--- src/main/java/cardibuddy/logic/commands/DeleteCommand.java
+++ src/main/java/cardibuddy/logic/commands/DeleteCommand.java
@@ -1,14 +1,52 @@
 package cardibuddy.logic.commands;
 
+import cardibuddy.commons.core.Messages;
+import cardibuddy.commons.core.index.Index;
 import cardibuddy.logic.commands.exceptions.CommandException;
 import cardibuddy.model.Model;
+import cardibuddy.model.deck.Deck;
+
+import java.util.List;
+
+import static cardibuddy.logic.parser.CliSyntax.PREFIX_Object;
+import static java.util.Objects.requireNonNull;
 
 public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Deletes a deck/flashcard identified by the index number used in the displayed cardibuddy book.\n"
+            + "Parameters: "
+            + PREFIX_Object + "Object Type INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_Object + "deck 1";
+
+    public static final String MESSAGE_DELETE_DECK_SUCCESS = "Deleted Deck: %1$s";
+
+    private final Index targetIndex;
+
+    public DeleteCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return null;
+        requireNonNull(model);
+        List<Deck> lastShownList = model.getFilteredDeckList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_DECK_DISPLAYED_INDEX);
+        }
+
+        Deck deckToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteDeck(deckToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_DECK_SUCCESS, deckToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeleteCommand // instanceof handles nulls
+                && targetIndex.equals(((DeleteCommand) other).targetIndex)); // state check
     }
 }
