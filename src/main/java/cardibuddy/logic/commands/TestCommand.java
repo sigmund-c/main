91ea7a3bd81fecbd80af43575ea26abc9ba8dfa8
phaebaez <50545929+phaebaez@users.noreply.github.com>
2020-03-16 20:46:40 +0800

    fixed styling errors

--- src/main/java/cardibuddy/logic/commands/TestCommand.java
+++ src/main/java/cardibuddy/logic/commands/TestCommand.java
@@ -5,7 +5,7 @@ import cardibuddy.model.Model;
 /**
  * Test Command
  */
-public class TestCommand extends Command{
+public class TestCommand extends Command {
 
     public static final String COMMAND_WORD = "test";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Started a test session with the given deck index.";
