2d9a1ce887066e0880ba1d275efc494fd67a00ce
phaebaez <50545929+phaebaez@users.noreply.github.com>
2020-03-16 20:36:51 +0800

    changed build.gradle file

--- src/main/java/cardibuddy/logic/commands/AddCommand.java
+++ src/main/java/cardibuddy/logic/commands/AddCommand.java
@@ -18,8 +18,8 @@ public class AddCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a deck/flashcard to the cardibuddy book. "
             + "Parameters: "
-            + PREFIX_DECK + "[Deck Name] "
-            + PREFIX_FLASHCARD + "[Card Name]"
+            + "[" + PREFIX_DECK + "Deck Name]... \n"
+            + "[" + PREFIX_FLASHCARD + "Flashcard Name]... \n"
 //            + PREFIX_TITLE + "Title \n"
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
