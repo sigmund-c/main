508ab094c6f661090ab1c37f9d7091e34dc36909
Ashleigh <ashleighaathar@gmail.com>
2020-04-10 23:10:57 +0800

    modify test session test

--- src/test/java/cardibuddy/model/testsession/TestSessionTest.java
+++ src/test/java/cardibuddy/model/testsession/TestSessionTest.java
@@ -3,10 +3,13 @@ package cardibuddy.model.testsession;
 import cardibuddy.model.deck.Deck;
 import cardibuddy.model.flashcard.Answer;
 import cardibuddy.model.flashcard.Card;
+import cardibuddy.model.testsession.exceptions.AlreadyCorrectException;
 import cardibuddy.model.testsession.exceptions.EmptyDeckException;
 import cardibuddy.model.testsession.exceptions.UnansweredQuestionException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -14,20 +17,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestSessionTest {
 
-    Deck deckTenCards = new DeckStub(0); // TODO: Configure deck
-    Deck deckOneCard = new DeckStub(1);
-    Deck deckEmpty = new DeckStub(0);
+    static Deck deckTenCards = new DeckStub(0); // TODO: Configure deck
+    static Deck deckOneCard = new DeckStub(1);
+    static Deck deckEmpty = new DeckStub(0);
 
     TestSession testSessionEmpty = new TestSession(deckEmpty);
     TestSession testSessionOneCard = new TestSession(deckOneCard);
     TestSession testSessionTenCards = new TestSession(deckTenCards);
-
-    TestSession testSessionFirstQuestionAnsweredCorrectly;
-    TestSession testSessionFirstQuestionAnsweredWrongly;
-    TestSession testSessionHalfCompleteUnansweredPrevAllCorrect;
-    TestSession testSessionHalfCompleteAnsweredCorrectly;
-    TestSession testSessionHalfCompleteAnsweredWrongly;
-
+    TestSession testSessionHalfComplete = getHalfCompleteTestSession(deckTenCards);
     TestSession testSessionEnded;
 
     /**
@@ -37,7 +34,7 @@ public class TestSessionTest {
      * It is not possible for the user to force correct.
      */
     static TestSession getHalfCompleteTestSession(Deck deck) {
-        int deckSize = ((DeckStub) deck).getFlashcardArrayList().size();
+        int deckSize = deck.getFlashcardList().size();
         int middle = deckSize/2;
         for (int i = 0; i < middle; i++) {
 
@@ -52,36 +49,48 @@ public class TestSessionTest {
     }
 
     /**
+     * Gets all the non empty decks.
+     */
+    static Deck[] getNonEmptyDecks() {
+        return new Deck[]{deckTenCards, deckOneCard};
+    }
+
+    /**
      * Get a test session with the
      */
 
     /**
      * Test the creation of the test queue in the constructor based on the given non empty deck.
      */
-    @ParameterizedTest
-    static <ParameterizedTest> void testConstructorWithNonEmptyDeck(Deck deck) {
-        TestSession testSession = new TestSession(deck);
-        LinkedList<Card> expectedTestQueue = ((DeckStub) deck).getExpectedTestQueue();
-        assertEquals(expectedTestQueue, testSession.getTestQueue());
+    @Test
+    static void testConstructorWithNonEmptyDeck() {
+        Deck[] decks = getNonEmptyDecks();
+        for (Deck deck : decks) {
+            TestSession testSession = new TestSession(deck);
+            LinkedList<Card> expectedTestQueue = ((DeckStub) deck).getExpectedTestQueue();
+            assertEquals(expectedTestQueue, testSession.getTestQueue());
+        }
     }
 
     /**
      * Ensure that an {@code EmptyDeckException} is thrown when an empty deck is passed to the constructor.
      */
     @Test
-    static void testConstructorWithEmptyDeck(Deck deck) {
-        TestSession testSession = new TestSession(deck);
-        assertThrows(EmptyDeckException.class, () -> new TestSession(deck));
+    static void testConstructorWithEmptyDeck() {
+        assertThrows(EmptyDeckException.class, () -> new TestSession(deckEmpty));
     }
 
     /**
      * Test that the correct first question is received when the deck given is non empty.
      */
     @Test
-    static void testGetFirstQuestionWithNonEmptyDeck(Deck deck) {
-        TestSession testSession = new TestSession(deck);
-        Card firstExpectedCard = ((DeckStub) deck).getFlashcardArrayList().get(0);
-        assertEquals(firstExpectedCard.getQuestion(), testSession.getFirstQuestion());
+    static void testGetFirstQuestionWithNonEmptyDeck() {
+        Deck[] decks = getNonEmptyDecks();
+        for (Deck deck : decks) {
+            TestSession testSession = new TestSession(deck);
+            Card firstExpectedCard = ((DeckStub) deck).getFlashcardArrayList().get(0);
+            assertEquals(firstExpectedCard.getQuestion(), testSession.getFirstQuestion());
+        }
     }
 
     /**
@@ -89,53 +98,63 @@ public class TestSessionTest {
      * Note that the actual testing of TestResult class will be in a separate test class itself.
      */
     @Test
-    static void testSubmitAnswerCreatesCorrectParameters(Deck deck) {
-        TestSession testSession = new TestSession(deck);
-        testSession.getFirstQuestion();
-        Card firstExpectedCard = ((DeckStub) deck).getFlashcardArrayList().get(0);
-        TestResult testResult = testSession.submitAnswer(firstExpectedCard.getAnswer().toString());
-        assertEquals(testResult.getNumTries(), 1);
-        assertEquals(testResult.getFlashcardAnswer(), firstExpectedCard.getAnswer());
-        assertEquals(testResult.getUserAnswer(), firstExpectedCard.getAnswer().toString());
+    static void testSubmitAnswerCreatesCorrectParameters() {
+        Deck[] decks = getNonEmptyDecks();
+        for (Deck deck : decks) {
+            TestSession testSession = new TestSession(deck);
+            testSession.getFirstQuestion();
+            Card firstExpectedCard = ((DeckStub) deck).getFlashcardArrayList().get(0);
+            TestResult testResult = testSession.submitAnswer(firstExpectedCard.getAnswer().toString());
+            assertEquals(testResult.getNumTries(), 1);
+            assertEquals(testResult.getFlashcardAnswer(), firstExpectedCard.getAnswer());
+            assertEquals(testResult.getUserAnswer(), firstExpectedCard.getAnswer().toString());
+        }
     }
 
     /**
      * Ensure that an {@code UnansweredQuestionException} is thrown when the user tries to get the next question without answering the current question.
-     * @param testSession
-     * @param deck
      */
     @Test
-    static void testUnansweredQuestionException(Deck deck) {
-        TestSession testSession = new TestSession(deck);
-        assertThrows(UnansweredQuestionException.class,  testSession::getNextQuestion);
+    static void testGetNextQuestionThrowsUnansweredQuestionException() {
+        Deck[] decks = getNonEmptyDecks();
+        for (Deck deck : decks) {
+            TestSession testSession = new TestSession(deck);
+            assertThrows(UnansweredQuestionException.class,  testSession::getNextQuestion);
+        }
     }
 
     /**
      * Ensure that an {@code AlreadyCorrectException} will be thrown if the user tries to skip a question when the user has already answered it correctly.
      */
     @Test
-    static void testAlreadyCorrectException(Deck deck) {
-
+    static void testSkipQuestionThrowsAlreadyCorrectException() {
+        Deck[] decks = getNonEmptyDecks();
+        for (Deck deck : decks) {
+            TestSession testSession = new TestSession(deck);
+            testSession.getFirstQuestion();
+            Card firstExpectedCard = ((DeckStub) deck).getFlashcardArrayList().get(0);
+            testSession.submitAnswer(firstExpectedCard.getAnswer().toString());
+            assertThrows(AlreadyCorrectException.class, testSession::skipQuestion);
+        }
     }
 
     /**
-     * Ensure that after submitting the answer, the boolean {@code }hasAnswered} will be toggled correctly and the user is able to move to the next question.
+     * Ensure that after submitting the answer, the boolean {@code hasAnswered} will be toggled correctly and the user is able to move to the next question.
      * Ensure that the size of the testQueue is decremented by 1 as the first question was answered correctly.
      * @param testSession
      * @param deck
      */
     @Test
-    static void testGetNextQuestion(TestSession answeredTestSession, Deck deck) {
+    static void testGetNextQuestion() {
 
     }
 
+    /**
+     * Ensure that after submitting the answer, when the user types next,
+     */
     @Test
-    static void testGetNextQuestion(TestSession testSession, Deck deck) {
+    static void testGetNextQuestionWithNoCardsLeft() {
 
     }
 
-    @Test
-    static void testAnsweringNoException() {
-
-    }
 }
